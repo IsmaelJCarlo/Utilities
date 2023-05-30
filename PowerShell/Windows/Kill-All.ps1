88aae446fb3be4c9bb0e6c11b8411f6baed71167
Ismael Carlo <IsmaelJCarlo@users.noreply.github.com>
2023-05-30 13:21:50 -0500

    Fixed error that was preventing the script from executing.

--- PowerShell/Windows/Kill-All.ps1
+++ PowerShell/Windows/Kill-All.ps1
@@ -1,11 +1,12 @@
 # A simple script that kills all processes matching the input ProcessName
+[CmdletBinding()]
+param(
+#  [Parameter(Mandatory,ParameterSetName='ByProcessName')]
+  [string]$ProcessName = $(Read-Host -Prompt 'Enter the process name'))
 
 function Kill-All {
-
-  [CmdletBinding()]
-  param(
-    [Parameter(Mandatory,ParameterSetName='ByProcessName')]
-    [string]$ProcessName)
-    
-  Get-Process | Where-Object -FilterScript {$_.processname -eq $ProcessName} | Select-Object id | kill
+  Write-Host -BackgroundColor Yellow -ForegroundColor Red "Process to kill: $ProcessName"  
+  Get-Process | Where-Object -FilterScript {$_.processname -eq $ProcessName} | Select-Object id | Kill-All
  }
+
+Kill-All -ProcessName $processName
